16388ca8ca14ca4c5241313458925e4bb0446891
kittinook <kitti.thamrong@gmail.com>
2023-10-05 08:02:51 +0700

    update

---
+++ HW_StaticForce/fra333_hw2_xxxx_xxxx_xxxx_xxxx.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+import numpy as np
+import math
+from HW2_utils import FKHW3
+
+'''
+    Name:   <NAME> <SURNAME> <ID>
+            <NAME> <SURNAME> <ID>
+    Date:   <DD-MM-YY>
+    Description:
+'''
+
+# Question 1
+def endEffectorJacobianHW3(q):
+    '''
+        q unit: rad
+        type something here
+
+        return format list 6x3
+        [ [i_11, i_12, i_13],
+          [i_21, i_22, i_23],
+          [i_31, i_32, i_33],
+          [i_41, i_42, i_43],
+          [i_51, i_52, i_53],
+          [i_61, i_62, i_63] ]
+    '''
+    
+
+# Question 2
+def checkSingularityHW3(q):
+    '''
+        q unit: rad
+        type something here
+        
+        return format bool
+    '''
+
+# Question 3
+def computeEffortHW3(q,w):
+    '''
+        q unit: rad
+        type something here
+
+        return format list 1x3
+        [ [i_11, i_12, i_13] ]
+    '''
